0daeb6eabde9a78e1e8e73786d54bfbed6c97562
Anton Korzunov <thekashey@gmail.com>
2021-06-13 16:02:51 +1000

    fix: allow function to be used as arguments

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 export const isWeakable = (value: any): boolean => (
   value && (typeof value === 'object')
-  // || typeof value === 'function' // not using function to prevent leaks
+  || typeof value === 'function'
 );
